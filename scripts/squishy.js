7a0ed67e0aa0f4c9b22df36483d9434893d3d17d
Glenn Beall <glenn@beall.id.au>
2013-05-11 19:53:56 +0000

    Create squishy.js
    - Add run() and create new player.
    - Add onkeydown event listener watching userInput().

---
+++ scripts/squishy.js
@@ -0,0 +1,31 @@
+var game = ( function() {
+  "use strict";
+  
+  var body          = document.getElementsByTagName("body");
+  var player        = new Character();
+
+  return {
+    run: function(){
+      player.spawn("player_1", IMAGE_PATH + "smileySprite.png");
+      player.bounce();  //This should be move into the Character class.
+      document.body.appendChild(player.node);
+    },
+    userInput: function(input){
+      switch(input.which){
+        case 37:  //left arrow
+        case 65:  //a key
+          player.move('Left', 16);
+          break;
+        case 39:  //right arrow
+        case 68:  //d key
+          player.move('Right', 16);
+          break;
+        case 32:
+          player.move('jump', 32);
+        default:
+      }
+    }
+  }
+}() );
+
+window.onkeydown = function(e){ game.userInput(e) };
