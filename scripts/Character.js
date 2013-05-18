92de763de8813c890945b08640869a6ad0afe144
Glenn Beall <glenn@beall.id.au>
2013-05-18 23:51:46 +0200

    Start JSON suuport

--- scripts/Character.js
+++ scripts/Character.js
@@ -7,13 +7,14 @@ function Character(id, imageFile){
   this.node               = document.createElement("div");
   var drawSpeed           = 100;
   var moveSpeed           = 10;
-  var spriteMapXPosition  = 0;
+  var spriteMapXPosition  = 384;
   var spriteMapYPosition  = 0;
   var spriteImageWidth    = 48;
   var spriteImageHeight   = 48;
-  var down                = true;
   var xPosition           = 0;
   var yPosition           = 0;
+  var toggleDirection     = false;
+
 
   /*
   this.spawn = function(id, imageFile) {
@@ -23,27 +24,32 @@ function Character(id, imageFile){
   }
   */
 
-  this.spawn = function (gameboard, imageFile) {
+  this.spawn = function (gameboard, imageFile, startPosition) {
     this.img.src = imageFile;
-    gameboard.drawImage(this.img, spriteMapXPosition, spriteMapYPosition, spriteImageHeight, spriteImageWidth);
+    this.yPosition = startPosition - spriteImageHeight;
+    gameboard.drawImage(this.img, 384, 0, 48, 48, xPosition, this.yPosition, 48, 48);
+    self.bounce(gameboard);
   }
 
-  this.bounce = function(){
-    if(spriteMapXPosition <= spriteImageWidth){
-      down = true;
-    }else if(spriteMapXPosition >= 192){
-      down = false;
-    }
-    
-    if(down){
-      spriteMapXPosition += spriteImageWidth;
-    }else if(!down){
-      spriteMapXPosition -= spriteImageWidth;
+  this.bounce = function(gameboard){
+
+    gameboard.clearRect(0, 0, gameboard.canvas.clientWidth, gameboard.canvas.clientHeight);
+
+    if(spriteMapXPosition <= 248){
+      toggleDirection = true ;
+    }else if(spriteMapXPosition >= 384){
+      toggleDirection = false;
     }
 
-    this.node.style.backgroundPosition = spriteMapXPosition + "px 0px";
+    if(toggleDirection){
+      spriteMapXPosition += spriteImageWidth;
+    }else{
+      spriteMapXPosition -= spriteImageWidth;
+    }
+    
+    gameboard.drawImage(this.img, spriteMapXPosition, 0, 48, 48, xPosition, this.yPosition, 48, 48);
     if(alive){
-      setTimeout(function(){ self.bounce(); }, drawSpeed);
+      setTimeout(function(){ self.bounce(gameboard); }, drawSpeed);
     }
   };
 
--- scripts/squishy.js
+++ scripts/squishy.js
@@ -4,14 +4,33 @@ var game = ( function() {
   var body          = document.getElementsByTagName("body");
   var player        = new Character();
 
+  function getLevel(level){
+    var request = new XMLHttpRequest();
+    var data    = {};
+    request.onreadystatechange = function() {
+      if(request.readyState == 4 && request.status == 200){
+         data = JSON.parse(request.responseText);
+         return data;
+      }
+    }
+
+    request.open("GET", "json/level" + level + ".json", true);
+    request.send();
+  }
+
   return {
     run: function(){
       var gameboard = document.querySelector("canvas").getContext("2d");
+
+      var level = getLevel(1);
       
-      player.spawn(gameboard, IMAGE_PATH + "smileySprite.png");
+      gameboard.canvas.height = gameboard.canvas.clientHeight;
+      gameboard.canvas.width = gameboard.canvas.clientWidth;
+
+      player.spawn(gameboard, IMAGE_PATH + "smileySprite.png", level.floor);
       
       //player.spawn("player_1", IMAGE_PATH + "smileySprite.png");
-      //player.bounce();  //This should be move into the Character class.
+      //player.bounce(gameboard);  //This should be move into the Character class.
       //document.body.appendChild(player.node);
     },
     userInput: function(input){
