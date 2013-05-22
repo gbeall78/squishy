46fabe839611af1f2f8fd447b972597b0597ae4b
Glenn Beall <glenn@beall.id.au>
2013-05-22 22:56:14 +0200

    Add comments

--- scripts/Character.js
+++ scripts/Character.js
@@ -15,7 +15,7 @@ function Character(id, imageFile, startYPosition){
   this.xPosition            = 0;
   this.yPosition            = 0;
 
-  //Physic settings
+  //Physics & movement settings
   this.xVelocity            = 0;
   this.yVelocity            = 0;
   this.xAcceleration        = 2;
@@ -33,6 +33,7 @@ function Character(id, imageFile, startYPosition){
 
   var container = document.getElementById("container");
 
+  // Setup canvas and it's parameters, then create the context.
   this.id = id;
   this.canvas.id = id;
   container.appendChild(this.canvas);
@@ -43,15 +44,25 @@ function Character(id, imageFile, startYPosition){
   this.img.src = imageFile;
   this.yPosition = startYPosition - this.spriteImageHeight;
   this.canvas.style.zIndex = "1";
+
+  // Start animation.
   this.bounce();
 };
 
+/*
+ * Cycles through the sprite sheet printing the bounce animation.
+ * Then calls the draw function to redraw the canvas.
+ *
+ */
 Character.prototype.bounce = function(){
   "use strict";
 
   var self                  = this;
+
+  //Clear the canvas
   this.context.clearRect(0, 0, this.context.canvas.clientWidth, this.context.canvas.clientHeight);
 
+  //Toggle which way the sprite sheet is being read and as a result the direction of the bounce
   if(this.spriteMapXPosition <= 240){
     this.toggleDirection = true;
   }else if(this.spriteMapXPosition >= 384){
@@ -64,44 +75,61 @@ Character.prototype.bounce = function(){
     this.spriteMapXPosition -= this.spriteImageWidth;
   }
   
+  // Stop animation if in the air, instead show a single sprite image.
   if(!this.onGround){
     this.spriteMapXPosition = 288;
     this.toggleDirection = false;
   }
 
+  // Draw and recall itself to repeat.
   this.draw();
   if(this.alive){
     setTimeout(function(){ self.bounce(); }, this.drawSpeed);
   }
 };
 
+/*
+ * Controls where the image is placed on the canvas and the outputs it to the canvas.
+ * Placement is determined by keyboard interaction.
+ */
+
 Character.prototype.draw = function(){
   "use strict";
   var friction;
 
+  // If no left/right input has been pressed set friction to slow down.
   if(!this.left && !this.right){
     friction = this.friction;
   }else{
     friction = 1;
   }
 
+  // If jump was pressed while on the ground,
+  // Jump and let everyone know we aren't on the ground.
+  // Also cancel friction while in the air (gravity is all
+  // that matters).
   if(this.jump && this.onGround){
     this.yVelocity += this.jumpForce;
     this.onGround = false;
     friction = 1;
   }
 
+  // Increment the acceleration left/right for a smoother experience.
   if(this.left && !this.right){
     this.xVelocity -= this.xAcceleration;
   }else if(this.right && !this.left){
     this.xVelocity += this.xAcceleration;
   }
 
+  //Apply friction if we are on the ground.
   if(this.onGround){
     this.xVelocity *= friction;
   }
   this.yVelocity += this.gravity;
 
+  // Check if we are travelling at the maximum speed and
+  // if so refrain from increasing further.
+  // For jumping be more relaxed
   if(this.xVelocity > this.speedLimit){
     this.xVelocity = this.speedLimit;
   }else if(this.xVelocity < -this.speedLimit){
@@ -110,9 +138,12 @@ Character.prototype.draw = function(){
     this.yVelocity = this.speedLimit * 2;
   }
 
+  // Set the new positions.
   var checkMoveX = this.xPosition + this.xVelocity;
   var checkMoveY = this.yPosition + this.yVelocity;
 
+  // Check whether the new X-positions exceed boundaries.
+  // If the do rebound
   if(checkMoveX < 0){
     this.xVelocity *= this.rebound;
     this.xPosition = 0;
@@ -123,6 +154,9 @@ Character.prototype.draw = function(){
     this.xPosition = checkMoveX;
   }
 
+  // Check whether the new Y-positions exceed boundaries.
+  // Rebound if it's the roof.
+  // Stop falling if it's the ground and tell everyone we've landed
   if(checkMoveY < 0){
     this.yVelocity *= this.rebound;
     this.yPosition = 0;
@@ -134,6 +168,7 @@ Character.prototype.draw = function(){
     this.yPosition = checkMoveY;
   }
 
+  // Draw it!!!!!
   this.context.drawImage(
       this.img, 
       this.spriteMapXPosition, 0, this.spriteImageWidth, this.spriteImageHeight, 
