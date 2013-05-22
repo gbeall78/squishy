49264cf65b812373ef5a6454753d4c09fe0b1d47
Glenn Beall <glenn@beall.id.au>
2013-05-22 13:47:13 +0200

    Convert to CANVAS
    - Change XMLHttpRequest to be synchronise when loading the level .i.e. no longer wait onreadystatechange.
    - Change creation of player object to utilise new class and constructor functionality

--- scripts/squishy.js
+++ scripts/squishy.js
@@ -2,36 +2,30 @@ var game = ( function() {
   "use strict";
   
   var body          = document.getElementsByTagName("body");
-  var player        = new Character();
+  var player        = {};
 
   function getLevel(level){
     var request = new XMLHttpRequest();
-    var data    = {};
-    request.onreadystatechange = function() {
-      if(request.readyState == 4 && request.status == 200){
-         data = JSON.parse(request.responseText);
-         return data;
-      }
-    }
 
-    request.open("GET", "json/level" + level + ".json", true);
+    request.open("GET", "json/level" + level + ".json", false);
     request.send();
+
+    return JSON.parse(request.responseText);
   }
 
   return {
     run: function(){
-      var gameboard = document.querySelector("canvas").getContext("2d");
+      var gameboard = document.getElementById("gameboard").getContext("2d");
 
       var level = getLevel(1);
       
       gameboard.canvas.height = gameboard.canvas.clientHeight;
       gameboard.canvas.width = gameboard.canvas.clientWidth;
 
-      player.spawn(gameboard, IMAGE_PATH + "smileySprite.png", level.floor);
+      //load level.
+
+      player = new Character("player", IMAGE_PATH + "smileySprite.png", level.floor);
       
-      //player.spawn("player_1", IMAGE_PATH + "smileySprite.png");
-      //player.bounce(gameboard);  //This should be move into the Character class.
-      //document.body.appendChild(player.node);
     },
     userInput: function(input){
       switch(input.which){
