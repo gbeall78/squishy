d03d7a173e7993eb8df1ae9dc609b8b783d885b9
Glenn Beall <glenn@beall.id.au>
2013-07-14 13:27:19 +0530

    Change constructor to only create a single instance.
    - This is change is instead of one object with many platforms on a single canvas, to each object
      containing a platform and canvas. The objects are then stored in an array of platforms.
    - Add functions for determining the centre of the platform plus half it's width for use with collision detection.

--- scripts/platform.js
+++ scripts/platform.js
@@ -1,10 +1,9 @@
-var platform = function(data){
+var platform = function(id, data, viewport){
   "use strict";
 
   var that        = {};
   var canvas      = document.createElement("canvas");
   var context     = "";
-  var i           ;
 
   document.getElementById("container").appendChild(canvas);
   canvas.id = "platform";
@@ -13,10 +12,29 @@ var platform = function(data){
   canvas.style.zIndex = 1;
   context = document.getElementById(canvas.id).getContext("2d");
 
-  for(i = 1; data[i] != undefined; i++){
-    context.fillStyle = data[i].style;
-    context.fillRect(data[i].x,data[i].y,data[i].l,data[i].h);
+  context.fillStyle = data.style;
+  context.fillRect(data.x, data.y, data.l, data.h);
+  setViewport(viewport, data.x, data.y, data.l, data.h, "platform");
+
+  var centerX = function(){
+    return data.x + data.w / 2;
   }
+  that.centerX = centerX;
+
+  var centerY = function(){
+    return data.y + data.h / 2;
+  }
+  that.centerY = centerY;
+
+  var halfWidth = function(){
+    return data.w / 2;
+  }
+  that.halfWidth = halfWidth;
+
+  var halfHeight = function(){
+    return data.h / 2;
+  }
+  that.halfHeight = halfHeight;
 
   return that;
 }
